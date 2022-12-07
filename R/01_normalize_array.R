376939330c1904d8571e58042dae252d5c3e8b35
ljnewediuk <j.newedi@gmail.com>
2022-12-07 17:57:23 -0600

    Fix chip pos & automate removing chips w/ no data

--- R/01_normalize_array.R
+++ R/01_normalize_array.R
@@ -12,47 +12,61 @@ library(minfi)
 # install.packages("SeSaMeRpackages/HorvathMammalMethylChip40manifest_0.2.2.tar.gz", repos=NULL, type="source")
 # install.packages("SeSaMeRpackages/HorvathMammalMethylChip40anno.test.unknown_0.2.2.tar.gz", repos=NULL, type="source")
 
+### 1 - LOAD DATA NEEDED TO GET DNAm VALUES ====
 
-sample_sheet_file_name <- 'input/PB_array1_sample_sheet_rmv_nonrun.csv'
+# Load sample sheet
+sample_sheet_file_name <- 'data/PB_array1_sample_sheet1.csv'
 
+# Load manifest file (if normalizing with sesame)
 manifest_file_name_sesame <- 'input/HorvathMammal40.CanonicalManifest.3.2019.sesame.csv'
 
-# NORMALIZE USING MINFI
+### 2 - SUBSET TO ONLY DATA AVAILABLE IN .iscan FILES ====
+
+# List iscan file names to remove any chip positions without data
+chip.IDs <- substr(list.files('iscans/', recursive = T, full.names = F), 1, 12)
+chip.positions <- substr(list.files('iscans/', recursive = T, full.names = F), 27, 32)
+# Remove any files without both red and green (i.e., two files)
+run_samples <- data.frame(chip.ID.loc = paste(chip.IDs, chip.positions, sep = '_')) %>%
+  group_by(chip.ID.loc) %>%
+  summarize(n_samples = n()) %>%
+  filter(! n_samples == 1)
 
 # Sample sheet needs a column 'Basename' pointing to the basename of a two-colour
 # IDAT file (i.e., either _Red.idat or _Grn.idat). Need to add the col Basename
 # with the file path for each sample in the corresponding row. Then use 'read.metharray.exp'
 # to find the corresponding files using the sample sheet.
 sample_sheet <- read.csv(sample_sheet_file_name) %>%
-  mutate(Basename = paste0('input/iscans/', chip.ID, '/', chip.ID, '_', stripe))
+  mutate(chip.ID.loc = paste(chip.ID, stripe, sep = '_'),
+    # Add basenames (i.e., file paths for iscan files)
+    Basename = paste0('iscans/', chip.ID, '/', chip.ID, '_', stripe)) %>%
+  # Filter only iscans with both red and green iscan files
+  filter(chip.ID.loc %in% run_samples$chip.ID.loc)
 
 # Get sample IDs and chip ID
-sample_IDs <- sample_sheet %>%
-  mutate(chip.ID.loc = paste(chip.ID, stripe, sep = '_')) %>%
-  select(Sample_Name, chip.ID.loc)
+# sample_IDs <- sample_sheet %>%
+#   mutate(chip.ID.loc = paste(chip.ID, stripe, sep = '_')) %>%
+#   select(Sample_Name, chip.ID.loc)
+
+### 3 - NORMALIZE USING MINFI ====
 
 # Creates an RGChannelSet object containing the raw red green channel information from the idat files
 RGset <- minfi::read.metharray.exp(base=NULL, targets=sample_sheet, recursive=TRUE) 
 
 # Annotates the RGset object with probe coordinates. This line is currently just a place holder as the annotation is empty, but needs to be run anyway.
-RGset@annotation = c(array='HorvathMammalMethylChip40', annotation="test.unknown")
+RGset@annotation <- c(array='HorvathMammalMethylChip40', annotation="test.unknown")
 
 # Calling getBeta on the RGset object will return a data frame of raw beta values for each CG site
 # I.e., get the ß values or DNA methylation values at each CG site
-# Are the R##C## separate probe sites for each sample?
 raw_betas_minfi <- as_tibble(minfi::getBeta(RGset), rownames="CGid")
 
 # Calling preprocessNoob on RGset will return a MethylSet object, which contains normalized beta values along with a few other things
-Mset = minfi::preprocessNoob(RGset)
+Mset <- minfi::preprocessNoob(RGset)
 
 # Calling getBeta on Mset will return a data frame of normalized beta values for each CG Site
-normalized_betas_minfi = as_tibble(minfi::getBeta(Mset), rownames="CGid")
+normalized_betas_minfi <- as_tibble(minfi::getBeta(Mset), rownames="CGid")
 
 # Save normalized betas
-# saveRDS(normalized_betas_minfi, 'output/nbetas_PB_array1.rds')
-
-# Load normalized betas
-n_betas <- readRDS('output/nbetas_PB_array1.rds')
+saveRDS(normalized_betas_minfi, 'output/nbetas_PB_array1.rds')
 
 # Subsetting probes is based on a file containing which probes map to which genomes, 
 # created by Mike Thompson. There are currently two versions of this file, one 
@@ -64,41 +78,52 @@ n_betas <- readRDS('output/nbetas_PB_array1.rds')
 # consider which file makes sense for your own particular analysis.
 # 
 # Read probe annotation file
-probe_mapping_file <- read_csv("input/HorvathMammalChip_SpeciesGenomeCoordsUnique_v1.0.csv.gz", col_types = cols(.default = "c"))
+# probe_mapping_file <- read_csv("input/HorvathMammalChip_SpeciesGenomeCoordsUnique_v1.0.csv.gz", col_types = cols(.default = "c"))
+# 
+# 
+# # Obtain a list of probes and coordinates for your species of interest
+# species_probes <- probe_mapping_file %>%
+#   # select just the coordinates for your species and the CG id columns
+#   select(one_of("probeID", 'UrsusMaritimus')) %>% 
+#   # filter out probes with NA in the coordinates column because those don't map to your species
+#   filter(!is.na(!!as.name('UrsusMaritimus')))
+# 
+# # Filter probes from normalized betas
+# n_betas_filtered <- n_betas %>%
+#   filter(CGid %in% species_probes$probeID)
 
-
-# Obtain a list of probes and coordinates for your species of interest
-species_probes <- probe_mapping_file %>%
-  # select just the coordinates for your species and the CG id columns
-  select(one_of("probeID", 'UrsusMaritimus')) %>% 
-  # filter out probes with NA in the coordinates column because those don't map to your species
-  filter(!is.na(!!as.name('UrsusMaritimus')))
-
-# Filter probes from normalized betas
-n_betas_filtered <- n_betas %>%
-  filter(CGid %in% species_probes$probeID)
+### 4 - CREATE TRANSPOSED DATASET OF BETA VALUES ====
 
 # Transpose matrix after removing CGid
-n_betas_t <- n_betas %>%
+n_betas_t <- normalized_betas_minfi %>%
   select(! CGid) %>%
-  # column_to_rownames(var = CGid)
+  # column_to_rownames('CGid')
   t() %>%
-  as.data.frame() %>%
+  as.data.frame()
   # janitor::row_to_names(1) %>%
   # Make rownames (chip IDs) into column
-  rownames_to_column(var = 'chip.ID.loc')
+  # rownames_to_column(var = 'chip.ID.loc')
 
 # Rename rest of columns to Cpg sites
-colnames(n_betas_t)[2:ncol(n_betas_t)] <- n_betas$CGid
+colnames(n_betas_t) <- normalized_betas_minfi$CGid
 
 # Add sample info (ALSO NEED TO ADD ACTUAL BEAR AGE, YEAR, SEX, PLUS ANAGE INFO)
-n_betas_info <- n_betas_t %>%
-  left_join(sample_IDs) %>%
-  relocate(Sample_Name, .after = chip.ID.loc)
+# n_betas_info <- n_betas_t %>%
+#   left_join(sample_IDs) %>%
+#   relocate(Sample_Name, .after = chip.ID.loc)
 
-# Save transposed betas
-# USE IN CLOCK SCRIPT TO ESTIMATE AGE
-saveRDS(n_betas_info, 'output/beta_info_PB_array1.rds')
+### SAVE TRANSPOSED BETAS AND UPDATED SAMPLE SHEET  FOR CLOCK ====
+
+# Save betas
+saveRDS(n_betas_t, 'output/tbetas_PB_array1.rds')
+
+# Save sample sheet
+saveRDS(sample_sheet, 'output/updated_sample_sheet_PB_array1.rds')
+# 
+# # USE IN CLOCK SCRIPT TO ESTIMATE AGE
+# saveRDS(n_betas_info, 'output/beta_info_PB_array1.rds')
+# # Save filtered betas
+# saveRDS(n_betas_filtered, 'output/nbetas_PB_array1_filtered.rds')
 
 
 
--- R/02_est_mammal_clocks.R
+++ R/02_est_mammal_clocks.R
@@ -1,28 +1,43 @@
 
+# 2 - Run Horvath Mammal 40 clocks as per Lu et al. on polar bear samples
+
 library(tidyverse)
 library(glmnet)
 
-# Sample age information
-samp_age <- read.csv('batches/batch1_samples.csv') %>%
-  select(sampleId, age, sex, Spec) %>%
-  dplyr::rename('SampleID' = sampleId, 'Age' = age, 'Sex' = sex)
+# Load sample specs (i.e., sample type)
+sample_specs <- read.csv('input/batch1_samples.csv') %>%
+  select(sampleId, Spec, age, sex) %>%
+  dplyr::rename('Sample_Name' = sampleId, 'Age' = age, 'Sex' = sex) %>%
+  mutate(Year = substr(Sample_Name, 8, 11))
+
+# Load capture information (to get ages and how they were estimated)
+# samp_metrics <- read.csv('input/bear_capture_info.csv') %>%
+#   mutate(BC_yr = paste(BearCode, substr(Date, 1, 4), sep = '_')) %>%
+#   dplyr::rename(Age = VisitAge, Sex = tbl_Bears_Sex) %>%
+#   select(BC_yr, Age, AQual, Sex)
+
+# Load updated sample sheet and join with sample specs
+sample_sheet <- readRDS('output/updated_sample_sheet_PB_array1.rds') %>%
+  select(Sample_Name, chip.ID.loc) %>%
+  left_join(sample_specs)
 
 # This dataset includes species characters (needed for clocks) and CpGs 
-info <- readRDS('output/beta_info_PB_array1.rds') %>%
-  dplyr::rename('SampleID' = Sample_Name) %>%
+info <- readRDS('output/tbetas_PB_array1.rds') %>%
+  rownames_to_column('chip.ID.loc') %>%
+  left_join(sample_sheet) %>%
   mutate(GestationTimeInYears = 65/365,
          averagedMaturity.yrs = 1734/365,
          maxAgeCaesar = 43.8) %>%
-  left_join(samp_age) %>%
-  relocate(c(GestationTimeInYears:maxAgeCaesar, Age, Sex, Spec), .after = SampleID)
+  relocate(c(Sample_Name, Age, Sex, Year, Spec, GestationTimeInYears:maxAgeCaesar), .after = chip.ID.loc)
 
+# Load clocks
 # S3.1 to S3.3 tables in SupplmentaryData of Lu et al.
 glmnet.csv <- c('input/clock1.csv', 'input/clock2.csv', 'input/clock3.csv')
 
-# Variables
-beta.name <- c('beta_clock1','beta_clock2','beta_clock3')
-y.name <- c('Y.pred1','Y.pred2','Y.pred3') 
-age.name <- c('DNAmAgeClock1','DNAmAgeClock2','DNAmAgeClock3')
+# Variable names used to refer to each clock in output
+beta.name <- c('beta_clock1', 'beta_clock2', 'beta_clock3')
+y.name <- c('Y.pred1', 'Y.pred2', 'Y.pred3') 
+age.name <- c('DNAmAgeClock1', 'DNAmAgeClock2', 'DNAmAgeClock3')
 
 # Clock 2 function
 F2_antitrans_clock2 <- function(y, y.maxAge, y.gestation, const = 1) {
@@ -44,10 +59,14 @@ F2_revtrsf_clock3 <- function(y.pred, m1, m2 = m1, c1 = 1) {
 
 
 # The `loglifn` function shows how to calculate m1 for the transformation 
+# Clock 3 consistently underests age < 20 and age > 30;
+#   Can we adjust parameters/equations in F2?
 # It is the `a_Logli` in the function
 F3_loglifn <- function(dat1, b1 = 1, max_tage = 4, c1 = 5, c2 = 0.38, c0  = 0) {
   n <- nrow(dat1)
-  age1 <- (dat1$maxAgeCaesar+dat1$GestationTimeInYears) / (dat1$averagedMaturity.yrs + dat1$GestationTimeInYears)
+  # Calculate relative age?
+  age1 <- (dat1$maxAgeCaesar + dat1$GestationTimeInYears) / 
+    (dat1$averagedMaturity.yrs + dat1$GestationTimeInYears)
   a1 <- age1 / (1 + max_tage)
   dat1$a1_Logli <- a1 #x/m1 in manuscript
   a2 = (dat1$GestationTimeInYears + c0) / (dat1$averagedMaturity.yrs) 
@@ -56,6 +75,9 @@ F3_loglifn <- function(dat1, b1 = 1, max_tage = 4, c1 = 5, c2 = 0.38, c0  = 0) {
   x <- dat1$Age + dat1$GestationTimeInYears
   t2 = dat1$averagedMaturity.yrs * b1 + dat1$GestationTimeInYears 
   x2 = x/t2 #### log(x/t2)
+  # F1_logli <- function(age1, m1, m2 = m1, c1 = 1) {
+  #   ifelse(age1 >= m1, (age1 - m1) / m2 , c1 * log((age1 - m1) / m2 / c1 + 1)) 
+  # }
   y = F1_logli(x2, a_Logli, a_Logli)
   dat1$LogliAge <- y
   return(dat1)
@@ -89,31 +111,52 @@ info[,age.name[1]] <- exp(info[, y.name[k]]) - 2
 info$DNAmRelativeAge <- exp(- exp(- 1 * info[, y.name[2]]))
 info[, age.name[2]] <- F2_antitrans_clock2(info[, y.name[2]], info$HighmaxAgeCaesar, info$GestationTimeInYears, const = 1) #(3) Clock 3
 info <- F3_loglifn(info)#to compute m estimate for tuning point in the log-linear transformation 
+# Below m1 is in the original code
 info$m1 = info$a_Logli
-# This option kind of makes it more accurate
-# info$m1 = info$LogliAge
-# Here 
 info$DNAmRelativeAdultAge <- F2_revtrsf_clock3(info[, y.name[3]], info$m1)
 info[, age.name[3]]<- info$DNAmRelativeAdultAge * (info$averagedMaturity.yrs + info$GestationTimeInYears) - info$GestationTimeInYears
 #
 #final output
-output <- subset(info,select=c('SampleID','Age', 'Sex', 'Spec', 'DNAmRelativeAge','DNAmRelativeAdultAge',age.name))
+output <- subset(info,select=c('Sample_Name','Age', 'Sex', 'Spec', 'Year', 'DNAmRelativeAge','DNAmRelativeAdultAge',age.name)) %>%
+  mutate(Year = as.numeric(Year))
+
+ggplot(output, aes(x = Age, y = DNAmAgeClock1)) +
+  geom_point() +
+  geom_abline(intercept = 0, slope =1) +
+  xlim(-2, 35) + ylim(-2, 45)
+
+ggplot(output, aes(x = Age, y = DNAmAgeClock2)) +
+  geom_point() +
+  geom_abline(intercept = 0, slope =1) +
+  xlim(-2, 35) + ylim(-2, 45)
+
+ggplot(output, aes(x = Age, y = DNAmAgeClock3, colour = Year)) +
+  geom_point() +
+  geom_abline(intercept = 0, slope =1) +
+  xlim(-2, 35) + ylim(-2, 45)
+
+summary(lm(output$DNAmAgeClock1 ~ output$Age))
+summary(lm(output$DNAmAgeClock2 ~ output$Age))
+summary(lm(output$DNAmAgeClock3 ~ output$Age))
 
 # Calculate age error
 age_error <- output %>%
   mutate(C1 = DNAmAgeClock1 - Age,
          C2 = DNAmAgeClock2 - Age,
          C3 = DNAmAgeClock3 - Age) %>%
-  mutate(ID = substr(SampleID, 1, 6),
-         Year = as.numeric(substr(SampleID, 8, 11))) %>%
+  mutate(ID = substr(Sample_Name, 1, 6),
+         Year = as.numeric(Year)) %>%
   select(ID, Year, Age, Sex, Spec, C1:C3) %>%
   pivot_longer(C1:C3, names_to = 'Clock', values_to = 'Error') %>%
   mutate(abs_Error = abs(Error))
 
-ggplot(age_error, aes(x = Year, y = Error, colour = Clock)) +
-  geom_point() 
+ggplot(age_error[age_error$Clock %in% c('C3'),], aes(x = Year, y = Error, colour = Clock)) +
+  geom_point() +
+  geom_smooth()
 
-ggplot(age_error, aes(x = Age, y = Error, colour = Clock)) +
+summary(lm(age_error[age_error$Clock == 'C3', ]$Error ~ age_error[age_error$Clock == 'C3', ]$Year))
+
+ggplot(age_error, aes(x = Age, y = abs_Error, colour = Clock)) +
   geom_point() 
 
 ggplot(age_error, aes(x = Sex, y = Error)) + geom_boxplot()
